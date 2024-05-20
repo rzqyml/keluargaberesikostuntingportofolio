ca123c00d0a148adf532e48d6c24a62850a6b7e6
Rizky Yaomal Malik <2010512081@mahasiswa.upnvj.ac.id>
2024-05-20 12:48:12 +0700

    Update streamlit_kbst.py

--- streamlit_kbst.py
+++ streamlit_kbst.py
@@ -9,6 +9,16 @@ kbst_model = pickle.load(open('kbst_model.sav', 'rb'))
 # Judul web
 st.title('SISTEM PREDIKSI KELUARGA BERESIKO STUNTING')
 
+# Membuat tombol dengan tautan
+link = 'https://drive.google.com/drive/folders/1GrdNf8OjWh8OWwudIN05ZCk2A4odqvAP?usp=sharing'
+button_spreadsheet = 'Download Sample Dataset'
+
+# Menggunakan HTML untuk membuat tautan
+if st.button(button_spreadsheet):
+    js = f"window.open('{link}')"  # Membuka tautan di tab baru
+    html = f"<script>{js}</script>"
+    st.markdown(html, unsafe_allow_html=True)
+
 # Upload file CSV
 uploaded_file = st.file_uploader("Unggah File Exel", type=["xlsx","xls"])
 
